64b253778d1c714bf145e20eddb0c0b53db961eb
shreyaa4 <shreyawagle70@gmail.com>
2024-07-14 15:24:05 +0530

    first commit

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,13 +1,14 @@
 
-alphabet=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-          'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-def encryption(plain_text,shift_key): 
+
+alphabet=['a','b','c','d','e','f','g','h','i','j','k','l','m',
+          'n','o','p','q','r','s','t','u','v','w','x','y','z']
+def encryption(plain_text,shift_key):
     cipher_text=""
     for char in plain_text:
         position=alphabet.index(char)
         new_position=(position+shift_key)%26
         cipher_text +=alphabet[new_position]
-    print (cipher_text)
+    print(f" The text after encryption: {cipher_text}")
 
 def decryption(cipher_text,shift_key): 
     plain_text=""
@@ -15,11 +16,11 @@ def decryption(cipher_text,shift_key):
         position=alphabet.index(char)
         new_position=(position-shift_key)%26
         plain_text +=alphabet[new_position]
-    print(plain_text)
+    print(f" The text after decryption: {plain_text}")
 
 end=False
 while not end:
-    what_to_do=input("Type 'encrypt' for encryption and  type 'decrypt' for decryption:\n")
+    what_to_do=input("Type 'encrypt' for encryption, type 'decrypt' for decryption:\n")
     text=input("Type your message:\n")
     shift=int(input("Enter shift key:\n"))
     if what_to_do=="encrypt":
@@ -28,4 +29,4 @@ while not end:
     elif what_to_do=="decrypt":
         decryption(text,shift)
         print("Decryption Complete")
-    
+   
