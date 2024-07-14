6a1cfd1b955cce1c522550f574066ceac0a2c7ae
shreyaa4 <shreyawagle70@gmail.com>
2024-07-14 13:21:25 +0530

    first commit

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,31 +1,31 @@
-letters = "abcdefghijklmnopqrstuvwxyz"
 
-text = input("Enter the text to be encrypted: ")
-key = 3
+alphabet=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+          'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+def encryption(plain_text,shift_key): 
+    cipher_text=""
+    for char in plain_text:
+        position=alphabet.index(char)
+        new_position=(position+shift_key)%26
+        cipher_text +=alphabet[new_position]
+    print (cipher_text)
 
-def encrypt(pt):
+def decryption(cipher_text,shift_key): 
+    plain_text=""
+    for char in cipher_text: 
+        position=alphabet.index(char)
+        new_position=(position-shift_key)%26
+        plain_text +=alphabet[new_position]
+    print(plain_text)
 
-    cipher_text = ""
-    for j in range(len(pt)):
-        for i in range(26):
-            if(pt[j] == letters[i]):
-                position = i
-                break
-        cipher = (position + key) % 26
-        cipher_text += letters[cipher]
-    return cipher_text
+end=False
+while not end:
+    what_to_do=input("Type 'encrypt' for encryption and  type 'decrypt' for decryption:\n")
+    text=input("Type your message:\n")
+    shift=int(input("Enter shift key:\n"))
+    if what_to_do=="encrypt":
+        encryption(plain_text=text,shift_key=shift)
+        print("Encryption Complete")
+    elif what_to_do=="decrypt":
+        decryption(text,shift)
+        print("Decryption Complete")
     
-def decrypt(ct):
-    plain_text = ""
-    for j in range(len(ct)):
-        for i in range(26):
-            if(ct[j] == letters[i]):
-                position = i
-                break
-        cipher = (position - key) % 26
-        plain_text += letters[cipher]
-    return plain_text
-
-
-print("Encrypted text:"+encrypt(text))
-print("Decrypted text:"+decrypt(encrypt(text)))
